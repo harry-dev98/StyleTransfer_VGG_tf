6972d0d1ec1753cdaec0b960606b3a741c846555
harry-dev98 <harry.769644@gmail.com>
2020-02-12 01:21:17 +0530

    inital commit

--- tf/vgg.py
+++ tf/vgg.py
@@ -50,9 +50,9 @@ class vgg():
             # Weights and Bias variables
             W = tf.get_variable('wts', shape = [h_filt, w_filt, input_channels,
                             num_filt],initializer = tf.random_normal_initializer(mean = 0.0, stddev = 0.01))
-            b = tf.get_variable('biases', shape = [num_filters], initializer = tf.constant_initializer(0.0))
+            b = tf.get_variable('biases', shape = [num_filt], initializer = tf.constant_initializer(0.0))
             #conv layer
-            conv = tf.nn.bias_add(tf.rnn.conv2d(x, W, strides=[1,stride,stride,1], paddings=pad, name=name), b)
+            conv = tf.nn.bias_add(tf.nn.conv2d(x, W, strides=[1,stride,stride,1], paddings=pad, name=name), b)
             a_relu = tf.nn.relu(conv)
         return a_relu
 #..reyaL-Conv>
@@ -137,13 +137,13 @@ class vgg():
         self.max_pool5 = self.max_pool(self.conv5_3, "pool5")
         # ..5kcolB>
         self.flattened = tf.reshape(self.max_pool5, [N, -1])
-        self.size = self.flattened.shape[-1]
+        size = self.flattened.shape[-1]
         # <Block6..
         self.fc6 = self.fc(self.flattened, size, 4096, name="fc6")
         self.dp6 = self.dropout(self.fc6, name="dropout6")
         # ..6kcolB>
         # <Block7..
-        self.fc7 = self.fc(dp6, 4096, 4096, name="fc7")
+        self.fc7 = self.fc(self.dp6, 4096, 4096, name="fc7")
         self.dp7 = self.dropout(self.fc7, name="dropout7")
         # ..7kcolB>
         # <Block8..
